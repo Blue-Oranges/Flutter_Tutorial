3ebce66bc977acb5a50a45330db06c7bcfd23def
ckjbug <1079091667@qq.com>
2019-02-21 01:45:18 +0800

    添加一些小控件

--- flutter_shop/lib/pages/category_page.dart
+++ flutter_shop/lib/pages/category_page.dart
@@ -3,7 +3,16 @@ class CategoryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child:Text('分类页面')),
+      body: Center(child:LinearProgressIndicatorDemo()),
+    );
+  }
+}
+class LinearProgressIndicatorDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      child: LinearProgressIndicator(),
     );
   }
 }
--- flutter_shop/lib/pages/member_page.dart
+++ flutter_shop/lib/pages/member_page.dart
@@ -2,8 +2,63 @@ import 'package:flutter/material.dart';
 class MemberPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child:Text('管理中心')),
+    return Column(
+      children: <Widget>[
+        Center(
+          child: Container(
+            margin: const EdgeInsets.all(10.0),
+            color: const Color(0xfff48fb1),
+            width: 160.0,
+            height: 160.0,
+          ),
+        ),
+        ContainerDefaultA(),
+        ContainerDefaultB()
+      ],
+    );
+  }
+}
+class ContainerDefaultA extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          width: 30.0,
+          height: 30.0,
+          child: Container(
+            width: 60.0,
+            height: 60.0,
+            color: const Color(0xfff48fb1),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ContainerDefaultB extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(
+        height: Theme.of(context).textTheme.display1.fontSize * 1.1 + 200.0,
+      ),
+      padding: const EdgeInsets.all(8.0),
+      color: Colors.teal.shade700,
+      alignment: Alignment.center,
+      child: Text('Hello World',
+          style: Theme.of(context)
+              .textTheme
+              .display1
+              .copyWith(color: Colors.white)),
+      foregroundDecoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage('https://www.example.com/images/frame.png'),
+          centerSlice: Rect.fromLTRB(270.0, 180.0, 1360.0, 730.0),
+        ),
+      ),
+      transform: Matrix4.rotationZ(0.1),
     );
   }
 }
